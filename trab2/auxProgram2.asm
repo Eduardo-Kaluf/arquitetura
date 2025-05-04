395b2be1beac9269312b55dec5e39377122b34ae
Eduardo-Kaluf <eduardokaluf1@gmail.com>
2025-05-04 04:29:21 -0300

    organizando

--- trab2/auxProgram2.asm
+++ trab2/auxProgram2.asm
@@ -1,10 +1,9 @@
 _program2:
-    ld r0, r0 ;; (r0, rx)
-    ld r1, r1 ;; (r0, ry)
+    ld r0, r0   ;; (r0, rx)
+    ld r1, r1   ;; (r0, ry)
     add r3, r0  ;; (rx + ry -> r3)
     st r3, r2
-    addi 0 ;; inc 1/all
+    inc r0, 1   ;; inc 1/all
     ji _program2
-
 
 ;; 0x20 0x25 0xcc 0x3e 0x71 0x1b
