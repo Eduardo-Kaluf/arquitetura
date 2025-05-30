d9fff08b0d037514b33cf492ae9617ea0fd09f30
Eduardo-Kaluf <eduardokaluf1@gmail.com>
2025-05-30 01:25:21 -0300

    Adicionando predicação, move, mult-ops, atualizando assembler

--- trab3/VLIW/assembler.py
+++ trab3/VLIW/assembler.py
@@ -1,4 +1,10 @@
 from enum import Enum
+import sys
+
+class Type(Enum):
+    R = 1
+    I = 2
+    M = 3
 
 def to_twos_complement(n: int, bits: int = 4) -> str:
     if n >= 0:
@@ -6,29 +12,54 @@ def to_twos_complement(n: int, bits: int = 4) -> str:
     else:
         return f"{(1 << bits) + n:0{bits}b}"
 
-class Type(Enum):
-    R = 1
-    I = 2
 
 # "INSTRUCTION": (TIPO, OPCODE)
 opcodes = {
-    "BRZR":     (Type.R.value, "0000"),
-    "BRZI":     (Type.I.value, "0001"),
-    "JR":       (Type.R.value, "0010"),
-    "RESERVED": (Type.R.value, "0011"),
-    "LD":       (Type.R.value, "0100"),
-    "ST":       (Type.R.value, "0101"),
-    "MOVH":     (Type.I.value, "0110"),
-    "MOVL":     (Type.I.value, "0111"),
-    "ADD":      (Type.R.value, "1000"),
-    "SUB":      (Type.R.value, "1001"),
-    "RESERVED": (Type.R.value, "1010"),
-    "OR":       (Type.R.value, "1011"),
-    "NOT":      (Type.R.value, "1100"),
-    "SLR":      (Type.R.value, "1101"),
-    "SRR":      (Type.R.value, "1110"),
-    "NOP":      (Type.R.value, "1111"),
+
+    # DADOS
+    "LD":        (Type.R.value, "0000"),
+    "ST":        (Type.R.value, "0001"),
+    "MOVH":      (Type.I.value, "0010"),
+    "MOVL":      (Type.I.value, "0011"),
+    "F-LD":      (Type.R.value, "0100"),
+    "F-ST":      (Type.R.value, "0101"),
+    "F-MOVH":    (Type.I.value, "0110"),
+    "F-MOVL":    (Type.I.value, "0111"),
+
+    # CONTROLE
+    "BRZR":      (Type.R.value, "0000"),
+    "BRZI":      (Type.I.value, "0001"),
+    "JR":        (Type.R.value, "0010"),
+    "MOV":       (Type.R.value, "0011"),  
+    "F-BRZR":    (Type.R.value, "0100"),
+    "F-BRZI":    (Type.I.value, "0101"),
+    "F-JR":      (Type.R.value, "0110"),
+    "F-MOV":     (Type.R.value, "0111"),
+    "SGT":       (Type.R.value, "1000"),
+    "SLT":       (Type.R.value, "1001"),
+    "SEQ":       (Type.R.value, "1010"),  
+    "STRUE":     (Type.R.value, "1011"),
+
+    # ARITMÉTICA E LÓGICA
+    "ADD":       (Type.R.value, "0000"),
+    "SUB":       (Type.R.value, "0001"),
+    "MULT-OP":   (Type.M.value, "0010"),
+    "OR":        (Type.R.value, "0011"),
+    "NOT":       (Type.R.value, "0100"),
+    "SLR":       (Type.R.value, "0101"),
+    "SRR":       (Type.R.value, "0110"),
+    "F-ADD":     (Type.R.value, "1000"),
+    "F-SUB":     (Type.R.value, "1001"),
+    "F-MULT-OP": (Type.M.value, "1010"),
+    "F-OR":      (Type.R.value, "1011"),
+    "F-NOT":     (Type.R.value, "1100"),
+    "F-SLR":     (Type.R.value, "1101"),
+    "F-SRR":     (Type.R.value, "1110"),
+
+    # NO OPERATION
+    "NOP":       (Type.R.value, "1111"),
 }
+
 
 registers = {
     "R0": "00",
@@ -37,7 +68,6 @@ registers = {
     "R3": "11",
 }
 
-import sys
 
 if len(sys.argv) == 2:
     assembly = sys.argv[1]
@@ -47,6 +77,7 @@ if len(sys.argv) == 2:
 else:
     print("Please provide just one .asm")
     exit(0)
+
 
 with open(assembly, "r") as inputFile:
     lines = [l for l in inputFile.readlines() if l != "\n"]
@@ -63,6 +94,8 @@ with open(assembly, "r") as inputFile:
             command = '11110000'
         elif opcodes[command_name][0] == Type.I.value:
             command = opcodes[command_name][1] + str(to_twos_complement(int(info[1]), 4))
+        elif opcodes[command_name][0] == Type.M.value:
+            command = opcodes[command_name][1] + registers[info[1].upper()] + str(to_twos_complement(int(info[2]), 2))
         else:
             command = opcodes[command_name][1] + registers[info[1].upper()] + registers[info[2].upper()]
 
