6316650bf263466c9c8ba7a7c98d9c49b0c7188c
sergingameprays <sergio.sivonei@hotmail.com>
2025-05-30 00:43:31 -0300

    compreto

---
+++ trab3/VLIW/completo.asm
@@ -0,0 +1,321 @@
+movh -1 
+nop
+sub r1 r1
+sub r2 r2
+
+movl -1
+nop
+nop
+sub r3 r3
+
+movl 4
+nop
+nop
+add r1 r0 
+
+nop
+nop
+nop
+sub r2 r0
+
+st r2 r1 
+nop
+nop 
+nop
+
+movh 6
+nop
+nop
+sub r1 r1
+
+movh 0
+nop
+nop
+add r1 r0
+
+movl 1
+nop
+nop
+add r1 r2
+
+nop
+nop
+nop
+add r3 r0
+
+nop
+nop
+sub r1 r3 
+sub r2 r3
+
+st r2 r1
+nop
+sub r1 r3 
+sub r2 r3
+
+st r2 r1
+nop
+sub r1 r3 
+sub r2 r3
+
+st r2 r1
+nop
+sub r1 r3 
+sub r2 r3
+
+st r2 r1
+nop 
+not r0 r2
+nop
+
+nop
+brzi -5
+nop
+nop
+
+
+movh -1
+nop
+nop
+nop
+
+movl -1
+nop
+nop
+nop
+
+ld r2 r0 
+nop 
+nop
+nop
+
+movh 1
+nop
+nop
+add r1 r2 
+
+movl 3 
+nop
+nop
+add r1 r2
+
+movh 0
+nop
+nop
+add r3 r0
+
+movl 0
+nop
+nop
+add r3 r2 
+
+nop
+nop
+nop
+not r0 r0
+
+nop
+nop
+sub r3 r0
+sub r1 r0
+
+st r3 r1
+nop
+sub r2 r0
+nop
+
+nop
+nop
+nop
+not r0 r2
+
+nop
+brzi -4
+nop
+nop
+
+movh 8
+nop 
+nop
+sub r3 r3
+
+movl 12
+nop
+nop
+nop
+
+movh -1
+nop 
+nop
+add r3 r0
+
+movl -2
+nop
+nop
+nop
+
+st r3 r0
+nop
+nop
+nop
+
+movl -1
+nop
+nop
+nop
+
+ld r2 r0 
+nop
+nop
+nop
+
+movl -3
+nop
+nop
+nop
+
+st r2 r0 
+nop
+nop
+nop
+
+movl -1 
+nop
+nop
+nop
+
+ld r2 r0
+nop
+nop
+nop
+
+nop
+nop
+add r1 r0 
+nop
+
+
+ld r0 r1 
+nop
+nop
+add r1 r2 
+
+ld r3 r1 
+nop
+nop
+add r1 r2 
+
+movh -1
+nop
+nop
+add r3 r0 
+
+st r3 r1
+nop
+nop
+sub r1 r2
+
+movl -1
+nop
+nop
+sub r1 r2 
+
+nop
+nop
+nop
+add r1 r0 
+
+ld r0 r1 
+nop
+nop
+add r1 r2 
+
+ld r3 r1 
+nop
+nop
+add r1 r2 
+
+movh -1
+nop
+nop
+add r3 r0 
+
+st r3 r1
+nop
+nop
+sub r1 r2
+
+movl -1
+nop
+nop
+sub r1 r2 
+
+nop
+nop
+nop
+add r1 r0 
+
+ld r0 r1 
+nop
+nop
+add r1 r2 
+
+ld r3 r1 
+nop
+nop
+add r1 r2 
+
+movh -1
+nop
+nop
+add r3 r0 
+
+st r3 r1
+nop
+nop
+sub r1 r2
+
+movl -2
+nop
+nop
+sub r1 r2 
+
+ld r3 r0 
+nop
+nop
+nop
+
+movl -3
+nop
+nop
+nop
+
+ld r2 r0 
+nop
+nop
+nop 
+
+nop
+nop
+nop
+add r2 r0 
+
+st r2 r0 
+nop
+nop
+not r0 r2
+
+movh -1
+brzr r0 r3
+nop
+nop
+
+nop
+nop
+nop
+sub r0 r0
+
+nop
+brzi 0
+nop 
+nop
