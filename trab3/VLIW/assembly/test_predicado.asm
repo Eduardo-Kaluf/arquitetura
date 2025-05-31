50fcf308a089c58b51237ed26ae7cfc2b61c8624
sergingameprays <sergio.sivonei@hotmail.com>
2025-05-31 14:42:04 -0300

    assembly puro do teste de predicado

---
+++ trab3/VLIW/assembly/test_predicado.asm
@@ -0,0 +1,59 @@
+nop
+strue
+nop
+nop
+
+nop
+nop
+mult-op r0 1
+sub r3 r3
+
+movl 10
+nop
+mult-op r1 2
+mult-op r2 2
+
+nop
+mov r1 r0
+add r3 r0
+not r2 r2
+
+nop
+sgt r1 r2
+mult-op r0 1 
+nop
+
+nop
+nop
+sub r1 r2
+nop
+
+nop
+f-mov r2 r1
+nop
+nop
+
+nop
+seq r1 r2
+nop
+nop
+
+f-st r3 r3 
+f-brzi 2
+f-add r2 r1
+sub r1 r1
+
+f-ld r2 r3
+nop
+nop
+nop
+
+nop 
+strue
+nop
+nop
+
+nop
+brzi 0
+nop
+nop
