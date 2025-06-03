13346afff892b2a64cf092fea879485525785e3d
Eduardo-Kaluf <eduardokaluf1@gmail.com>
2025-06-02 22:42:08 -0300

    Fix moves e test scalar

---
+++ trab3/Vetorial/assembly/testScalarInstructions.asm
@@ -0,0 +1,31 @@
+s.movl 5
+s.add r2 r1
+s.brzr r0 r1
+s.movl 3
+s.movl 3
+s.movh 1
+s.brzr r1 r1
+s.sub r1 r2
+
+s.sub r1 r1
+s.sub r2 r2
+s.sub r3 r3
+
+s.movl 3
+s.add r2 r1
+s.movl 5
+s.st r2 r1
+s.movl 2
+s.add r2 r1
+s.ld r1 r2
+
+s.sub r1 r1
+s.sub r2 r2
+s.sub r3 r3
+
+s.movl 5
+s.and r1 r2
+s.movl 3
+s.add r2 r1
+s.and r1 r2
+
