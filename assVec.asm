2f9fe247d77c7801bf694f4ea0a2c60e26e4e9c3
sergingameprays <sergio.sivonei@hotmail.com>
2025-06-10 00:13:58 -0300

    ai ó

---
+++ assVec.asm
@@ -0,0 +1,145 @@
+
+s.and r2 r0
+s.and r3 r0
+
+s.movh 1
+s.movl 14
+
+s.add r3 r1
+
+s.movh 0
+s.movl 3
+s.add r2 r1
+
+v.sub r2 r2
+v.sub r3 r3
+
+v.movh 0 
+v.movl 11
+v.add r2 r1
+v.add r3 r1
+
+v.movh 6
+v.movl 4
+
+v.add r3 r1
+v.movh 0
+
+v.sub r3 r0 
+
+v.sub r2 r0 
+
+s.brzr r2 r3
+
+s.movh 0
+s.movl 1
+
+v.st r2 r3
+
+v.sub r3 r1
+v.sub r2 r1
+
+s.sub r2 r1
+
+s.movh 1
+s.movl 4
+
+s.brzr r0 r1
+
+
+s.add r3 r1 
+
+s.movh 0
+s.movl 3
+
+
+s.add r2 r1
+
+s.add r3 r1
+s.add r3 r1
+
+v.movh 1
+v.movl 8
+
+v.add r3 r1
+v.sub r2 r2
+
+v.movl 15
+v.add r2 r1
+v.sub r2 r0 
+
+v.movh 0
+s.movh 0
+v.movl 4
+
+s.brzr r2 r3 
+
+s.movh 0
+s.movl 1
+s.sub r2 r1
+
+v.st r2 r3
+
+v.sub r3 r1
+v.sub r2 r1
+
+s.movh 2
+s.movl 14
+
+s.brzr r0 r1
+
+
+S.movh 1
+S.movl 10
+
+S.add r3 r1 
+
+S.movh 0
+s.movl 3
+
+S.add r2 r1
+
+
+s.Brzr r2 r3
+
+S.movh 0
+
+S.movl 1
+S.sub r2 r1
+
+V.movl 12
+V.ld r2 r3
+
+V.add r3 r1 
+
+V.ld r1 r3 
+
+V.add r2 r1
+
+v.movh 0
+
+V.movl 12 
+
+V.add r3 r1 
+
+V.st r2 r3
+
+V.sub r3 r1
+
+V.sub r3 r1
+
+V.movl 4
+
+V.sub r3 r1 
+
+S.Movh 3
+
+S.Movl 14
+
+S.Brzr r0 r1
+
+s.Movh 5
+
+s.Movl 4
+
+s.Brzr r0 r1
